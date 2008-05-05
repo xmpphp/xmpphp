6383d9b980f076f34ae041964deced3c2e064228
temp <temp@ef36c318-a008-4979-b6e8-6b496270793b>
2008-05-05 18:35:48 +0000

    fixed changed log constants in examples
    git-svn-id: svn://netflint.net/xmpphp@33 ef36c318-a008-4979-b6e8-6b496270793b

--- sendmessage_example.php
+++ sendmessage_example.php
@@ -5,7 +5,7 @@ error_reporting(E_ALL & E_STRICT);
 
 include("XMPPHP/XMPP.php");
 
-$conn = new XMPP('talk.google.com', 5222, 'username', 'password', 'xmpphp', 'gmail.com', $printlog=false, $loglevel=XMPPHP_Log::LEVEL_INFO);
+$conn = new XMPPHP_XMPP('talk.google.com', 5222, 'username', 'password', 'xmpphp', 'gmail.com', $printlog=false, $loglevel=XMPPHP_Log::LEVEL_INFO);
 $conn->connect();
 $conn->processUntil('session_start');
 $conn->presence();
