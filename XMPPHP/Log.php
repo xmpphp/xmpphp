e9d36af5953e85c2781b88304778c0437b2b3ce5
fritzy <fritzy@ef36c318-a008-4979-b6e8-6b496270793b>
2008-11-21 08:11:59 +0000

    * Why is logging recording all of the logs forever?  Fixed memory leak.
    git-svn-id: svn://netflint.net/xmpphp@65 ef36c318-a008-4979-b6e8-6b496270793b

--- XMPPHP/Log.php
+++ XMPPHP/Log.php
@@ -84,7 +84,7 @@ class XMPPHP_Log {
 	 */
 	public function log($msg, $runlevel = self::LEVEL_INFO) {
 		$time = time();
-		$this->data[] = array($this->runlevel, $msg, $time);
+		#$this->data[] = array($this->runlevel, $msg, $time);
 		if($this->printout and $runlevel <= $this->runlevel) {
 			$this->writeLine($msg, $runlevel, $time);
 		}
